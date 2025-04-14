dd89b169fcf415072a98ffb45ea49cb2ab06336c
xht <1169740905@qq.com>
2025-04-14 23:10:35 +0800

    4.14修改

--- react-antd-admin/src/App.js
+++ react-antd-admin/src/App.js
@@ -1,7 +1,5 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { setDataSource } from './store/reducers/dataSourceSlice'
 import _ from "lodash";
 import User from "./pages/setting/user/index";
 import Business from "./pages/business/product/index";
@@ -13,32 +11,36 @@ import LoginPage from './pages/login/index'
 const Page404 = () => <div>404 Not Found</div>;
 
 function App() {
-
-  const dispatch = useDispatch()
-  const dataSource = useSelector(state => state.dataSource.dataSource);
-
-
-
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<HomeDiv />}>
-            <Route index element={<Home />} />
-            <Route path="/setting/menu" element={<Menu />} />
-            <Route path="/setting/user" element={<User />} />
-            <Route path="/business/product" element={<Business />} />
-            <Route path="/business/overview" element={<OverView />} />
-            <Route path="*" element={<Page404 />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<div>Loading Layout...</div>}>
+                  <HomeDiv />
+                </Suspense>
+              }
+            >
+              <Route index element={<Home />} />
+              <Route path="/setting/menu" element={<Menu />} />
+              <Route path="/setting/user" element={<User />} />
+              <Route path="/business/product" element={<Business />} />
+              <Route path="/business/overview" element={<OverView />} />
+              <Route path="*" element={<Page404 />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
 }
 
 export default App;
+
 
 
 
